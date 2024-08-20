ebd2800679dbc101529c9fd40ab9df2cb92ad73b
Ryan Brue <ryanbrue.dev@gmail.com>
2024-08-20 01:00:44 -0500

    add --no-warn, --clean options
    Signed-off-by: Ryan Brue <ryanbrue.dev@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -17,30 +17,59 @@ struct ConsoleArgs {
     /// Optional output path override. Defaults to ./output
     #[arg(short)]
     output_path: Option<PathBuf>,
+    /// Clean the output directory before generating the site. Useful for multiple runs
+    #[arg(long)]
+    clean: bool,
+    /// Disallow any warnings
+    #[arg(long)]
+    no_warn: bool,
 }
 
 fn main() -> anyhow::Result<()> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("warn")).init();
     log::trace!("Begin djot-ssg::main()");
     let args = ConsoleArgs::parse();
-    generate_site(
-        &args.target_path,
-        &args
-            .output_path
-            .unwrap_or(env::current_dir()?.join("output")),
-    )?;
+    let output_path = args
+        .output_path
+        .unwrap_or(env::current_dir()?.join("output"));
+    // Clean the output directory if clean is specified
+    if args.clean {
+        log::debug!(
+            "Clean argument specified, cleaning output path {:?}...",
+            &output_path
+        );
+        if let Err(e) = std::fs::remove_dir_all(&output_path) {
+            log::warn!("Cleanup failed: {}", e);
+        } else {
+            log::trace!("Clean successful!");
+        }
+    }
+    generate_site(&args.target_path, &output_path, args.no_warn)?;
     Ok(())
 }
 
-fn generate_site(target_path: &Path, output_path: &Path) -> anyhow::Result<()> {
+fn generate_site(target_path: &Path, output_path: &Path, no_warn: bool) -> anyhow::Result<()> {
     let _ = std::fs::create_dir_all(output_path);
+    log::trace!(
+        "Created output directory {:?} if it didn't exist...",
+        output_path
+    );
+    if !utils::check_has_index(target_path) {
+        warn_or_error!(no_warn, "index.{{dj|djot}} not found! consider creating one in the base target directory as the default page.");
+    }
     for entry in WalkDir::new(target_path) {
         match entry {
             Ok(direntry) => {
+                if direntry.path().is_dir() {
+                    log::trace!("Path {:?} is a directory, continuing...", direntry.path());
+                    continue;
+                }
                 log::trace!("Path: {:?}", direntry.path());
                 let new_path = match utils::get_relative_path(direntry.path(), &target_path) {
                     Some(relative) => output_path.join(relative),
                     None => {
-                        log::warn!(
+                        warn_or_error!(
+                            no_warn,
                             "Path {:?} is not relative to {:?}, skipping...",
                             direntry.path(),
                             &target_path
@@ -50,7 +79,7 @@ fn generate_site(target_path: &Path, output_path: &Path) -> anyhow::Result<()> {
                 };
                 let _ = std::fs::create_dir_all(&new_path.parent().unwrap());
                 match direntry.path().extension().map(|x| x.to_str().unwrap()) {
-                    Some(".dj") | Some(".djot") => {
+                    Some("dj") | Some("djot") => {
                         let result_path = new_path.with_extension("html");
                         log::debug!(
                             "Generating .html from {:?} and moving to {:?}",
@@ -68,9 +97,15 @@ fn generate_site(target_path: &Path, output_path: &Path) -> anyhow::Result<()> {
                 }
             }
             Err(e) => {
-                log::warn!("An entry returned error {e}");
+                warn_or_error!(no_warn, "An entry returned error {}", e);
             }
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn generate_sample_site() {}
 }
--- src/utils.rs
+++ src/utils.rs
@@ -10,3 +10,27 @@ pub fn get_relative_path(full_path: &Path, base_path: &Path) -> Option<PathBuf> 
         None
     }
 }
+
+pub fn check_has_index(target_path: &Path) -> bool {
+    target_path.join("index.dj").exists() || target_path.join("index.djot").exists()
+}
+
+#[macro_export]
+macro_rules! warn_or_error {
+    // This pattern matches when there is a format string followed by arguments
+    ($no_warn:expr, $fmt:expr, $($arg:tt)*) => {
+        if $no_warn {
+            return Err(anyhow::anyhow!($fmt, $($arg)*))
+        } else {
+            log::warn!($fmt, $($arg)*);
+        }
+    };
+    // This pattern matches when there is only a format string (no arguments)
+    ($no_warn:expr, $fmt:expr) => {
+        if $no_warn {
+            return Err(anyhow::anyhow!($fmt))
+        } else {
+            log::warn!($fmt);
+        }
+    };
+}
